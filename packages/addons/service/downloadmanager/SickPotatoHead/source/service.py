b6d3173f204872e833269dedfa4c9d28a9a17711
lsellens <lsellens@gmail.com>
2013-08-24 19:48:08 -0500

    Fix for some files that were missing

--- packages/addons/service/downloadmanager/SickPotatoHead/source/service.py
+++ packages/addons/service/downloadmanager/SickPotatoHead/source/service.py
@@ -14,11 +14,9 @@ __cwd__        = __settings__.getAddonInfo('path')
 __start__      = xbmc.translatePath( os.path.join( __cwd__, 'bin', "SickPotatoHead.py") )
 __stop__       = xbmc.translatePath( os.path.join( __cwd__, 'bin', "SickPotatoHead.stop") )
 
-#xbmc.executebuiltin("XBMC.Notification('SickPotatoHead', 'Starting Services', '500', %s)" % ( __cwd__ + '/icon.png'))
 subprocess.call(['python',__start__])
 
 while (not xbmc.abortRequested):
     time.sleep(0.250)
 
-#xbmc.executebuiltin("XBMC.Notification('SickPotatoHead', 'Shuting down Services', '5000', %s)" % ( __cwd__ + '/icon.png'))
 subprocess.Popen(__stop__, shell=True, close_fds=True)

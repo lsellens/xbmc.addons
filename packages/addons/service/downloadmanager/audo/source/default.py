0055f897cff0c2a2b658ee6c8eea299d10d6b49e
lsellens <lsellens@gmail.com>
2014-03-29 21:06:36 -0500

    Forking SABnzbd-Suite to new addon called audo

---
+++ packages/addons/service/downloadmanager/audo/source/default.py
@@ -0,0 +1,16 @@
+#
+import os
+import xbmc
+import xbmcaddon
+
+__scriptname__ = "audo"
+__author__     = "lsellens"
+__url__        = "http://code.google.com/p/repository-lsellens/"
+__settings__   = xbmcaddon.Addon(id='service.downloadmanager.audo')
+__cwd__        = __settings__.getAddonInfo('path')
+__start__      = xbmc.translatePath( os.path.join( __cwd__, 'bin', "audo.py") )
+__stop__       = xbmc.translatePath( os.path.join( __cwd__, 'bin', "audo.stop") )
+
+#Open settings dialog
+if __name__ == '__main__':
+    __settings__.openSettings()

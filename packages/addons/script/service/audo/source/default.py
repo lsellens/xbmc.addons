0edb34733507cc30726014391b745aec447b3822
lsellens <lsellens@gmail.com>
2014-04-10 19:25:42 -0500

    updates

--- packages/addons/script/service/audo/source/default.py
+++ packages/addons/script/service/audo/source/default.py
@@ -1,15 +1,10 @@
 #
-import xbmc
 import xbmcaddon
 
 __scriptname__ = "audo"
 __author__     = "lsellens"
 __url__        = "http://code.google.com/p/repository-lsellens/"
 __addon__      = xbmcaddon.Addon(id='script.service.audo')
-__cwd__        = __addon__.getAddonInfo('path')
-__addondir__   = __addon__.getAddonInfo('profile')
-__start__      = xbmc.translatePath(__cwd__ + '/bin/audo.py')
-__stop__       = xbmc.translatePath(__cwd__ + '/bin/audo.stop') 
 
 #Open settings dialog
 if __name__ == '__main__':

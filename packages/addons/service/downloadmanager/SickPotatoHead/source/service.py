954aa070d1f79e944481b8def37567fc389ec5ed
lsellens <lsellens@gmail.com>
2014-05-07 17:50:43 -0500

    updates to sickpotatoheads structure

--- packages/addons/service/downloadmanager/SickPotatoHead/source/service.py
+++ packages/addons/service/downloadmanager/SickPotatoHead/source/service.py
@@ -2,7 +2,6 @@
 import xbmc
 import xbmcaddon
 import time
-import subprocess
 
 __scriptname__ = "SickPotatoHead"
 __author__     = "lsellens"
@@ -10,11 +9,11 @@ __url__        = "http://code.google.com/p/repository-lsellens/"
 __addon__      = xbmcaddon.Addon(id='service.downloadmanager.SickPotatoHead')
 __addonpath__  = __addon__.getAddonInfo('path')
 __start__      = xbmc.translatePath(__addonpath__ + '/bin/SickPotatoHead.py')
-__stop__       = xbmc.translatePath(__addonpath__ + '/bin/SickPotatoHead.stop')
+__stop__       = xbmc.translatePath(__addonpath__ + '/bin/SickPotatoHead.stop.py')
 
 xbmc.executebuiltin('XBMC.RunScript(%s)' % __start__, True)
 
 while not xbmc.abortRequested:
     time.sleep(0.250)
 
-subprocess.Popen(__stop__, shell=True, close_fds=True)
+xbmc.executebuiltin('XBMC.RunScript(%s)' % __stop__, True)
